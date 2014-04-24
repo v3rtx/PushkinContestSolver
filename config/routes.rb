4f8d8b68c544ec71cf34ce8e55675aa5cd958941
v3rtx <den.groove@gmail.com>
2014-04-24 03:00:56 +0300

    Fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Pushkin::Application.routes.draw do
   get "home/hello"
   get 'parse/do'
-  root 'parse#do'
+  root 'home#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
